0b86ebcfe15ae55db5eabf4bd664611dabaa44f3
Ling-Hao CHEN <48591455+LinghaoChan@users.noreply.github.com>
2024-11-04 11:11:20 +0800

    remove ffmpeg path

--- utils/plot_script.py
+++ utils/plot_script.py
@@ -111,7 +111,7 @@ def plot_3d_motion(save_path, kinematic_tree, joints, title, figsize=(10, 10), f
     ani = FuncAnimation(fig, update, frames=frame_number, interval=1000 / fps, repeat=False)
 
     from matplotlib import rcParams
-    rcParams['animation.ffmpeg_path'] = '/home/chenlinghao/software/miniconda3/envs/py10/bin/ffmpeg' 
+    
     writer = FFMpegFileWriter(fps=fps)
     ani.save(save_path, writer=writer)
     plt.close()
